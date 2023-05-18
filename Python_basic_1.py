49c57e04d77908f75fc6e12886ed62ba8500986d
vicky-duff2002 <133871779+vicky-duff2002@users.noreply.github.com>
2023-05-18 12:02:30 +0100

    Create Python_basic_1.py
    Assignment 1

---
+++ Python_basic_1.py
@@ -0,0 +1,4 @@
+name = input ("what is your name\t")
+age = input ("how old are you\t")
+number= input ("what is your phone number\t")
+print ("hello" + (name), "you are\t" + (age) + "years old", "with the phone number [{}]". format (number))
